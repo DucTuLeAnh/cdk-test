d22a01f849eee6e69c64f2f098168cfd6378462a
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2024-07-27 17:33:32 +0200

    feat(config): add L2 fargate setup

---
+++ lib/cdk-test-stack.ts
@@ -0,0 +1,98 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { FargateTaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+
+export class CdkTestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const ECS_BACKEND_REPO_ARN: string = process.env.ECS_BACKEND_REPO_ARN ?? ""
+    const ECS_BACKEND_TASK_EXECUTION_ROLE: string = process.env.ECS_BACKEND_TASK_EXECUTION_ROLE ?? ""
+
+    const vpc = new ec2.Vpc(this, 'my-test-vpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      availabilityZones: ["eu-central-1a", "eu-central-1b"],
+      enableDnsHostnames: false
+
+    });
+
+    const ecsBackendSecurityGroup = new ec2.SecurityGroup(this, 'ecs-backend-sg', {
+      vpc: vpc,
+      allowAllOutbound: true
+    });
+
+    //TODO: check out security best practices in terms of allowing IP's
+    ecsBackendSecurityGroup.addIngressRule(
+      ecsBackendSecurityGroup,
+      ec2.Port.allTraffic(),
+      'Allow all inbound traffic from within the security group'
+    );
+
+    ecsBackendSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5000),
+      'Allow all inbound traffic on port 5000'
+    );
+    ecsBackendSecurityGroup.addEgressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5000),
+      'Allow all TCP connections on port 5000 to destination 0.0.0.0/0'
+    );
+
+    const ecsCluster = new ecs.Cluster(this, "ecs-test-cluster", {
+      vpc: vpc
+    });
+
+    const backendRepository = ecr.Repository.fromRepositoryAttributes(this, 'test-repo', {
+      repositoryArn: ECS_BACKEND_REPO_ARN,
+      repositoryName: 'test',
+    });
+
+    const ecsBackendTaskExecutionRole = iam.Role.fromRoleArn(this, 'ecsTaskExecutionRole', ECS_BACKEND_TASK_EXECUTION_ROLE, {
+      mutable: false
+    });
+
+    const ecsBackendLoadBalancer = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const ecsBackendLBListener = ecsBackendLoadBalancer.addListener('Listener', {
+      port: 80,
+      open: true
+    });
+
+    const ecsBackendTargetGroup = ecsBackendLBListener.addTargets('ApplicationFleet', {
+      port: 5000,
+      protocol: ApplicationProtocol.HTTP
+    });
+
+    const ecsBackendTaskDefinition = new FargateTaskDefinition(this, 'DefaultTask', {
+      family: 'DefaultTask',
+      taskRole: ecsBackendTaskExecutionRole,
+      executionRole: ecsBackendTaskExecutionRole,
+    });
+
+    ecsBackendTaskDefinition.addContainer('test-backend-container', {
+      image: ecs.ContainerImage.fromEcrRepository(backendRepository, 'latest'),
+      portMappings: [{ hostPort: 5000, containerPort: 5000, protocol: ecs.Protocol.TCP }],
+      environment: { "SD_HOST_NAME": "test" }
+    });
+
+    const service = new ecs.FargateService(this, 'Service', {
+      cluster: ecsCluster,
+      taskDefinition: ecsBackendTaskDefinition,
+      desiredCount: 1,
+      assignPublicIp: false,
+      securityGroups: [ecsBackendSecurityGroup],
+    });
+
+    ecsBackendTargetGroup.addTarget(service)
+  }
+}
